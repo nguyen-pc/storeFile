a5fd971ae124989effd4ffa25829bf2c50e66118
nguyen <nguyenthcs430@gmail.com>
2024-12-07 20:47:03 +0700

    detail file

---
+++ components/ActionsDropdown.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { Dialog } from "@/components/ui/dialog";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { actionsDropdownItems } from "@/constants";
+import Image from "next/image";
+import { Models } from "node-appwrite";
+import { useState } from "react";
+
+const ActionsDropdown = ({ file }: { file: Models.Document }) => {
+  const [isModelOpen, setIsModelOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  return (
+    <Dialog open={isModelOpen} onOpenChange={setIsModelOpen}>
+      <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
+        <DropdownMenuTrigger className="shad-no-focus">
+          <Image
+            src="/assets/icons/dots.svg"
+            alt="dots"
+            width={34}
+            height={34}
+          />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel className="max-w-[200px] truncate">
+            {file.name}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          {actionsDropdownItems.map((actionItem) => (
+            <DropdownMenuItem key={actionItem.value}>
+              {actionItem.label}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </Dialog>
+  );
+};
+
+export default ActionsDropdown;
